e0c669d00e6e6a49256a17a1bc76decb8b003c23
vmalvarezcamarero <vmalvarezcamarerogmail.com>
2021-02-17 11:44:20 +0100

    nuevo

---
+++ hello/exercises.py
@@ -0,0 +1,13 @@
+
+
+series = [0, 1, 1]
+
+a = 1
+b = 1
+
+for e in range (0, 12):
+    f = b + a
+    series.append(f)
+    a = b
+    b = f
+    print(series[e])
--- hello/hello.py
+++ hello/hello.py
@@ -1,1 +1,2 @@
-print("Hi")
+print("hi")
+print("hello")
