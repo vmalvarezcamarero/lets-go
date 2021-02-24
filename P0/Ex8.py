bf5448afa0c3cf3e95035ae214fd38f890c29ef6
vmalvarezcamarero <vmalvarezcamarerogmail.com>
2021-02-24 21:15:35 +0100

    ex 7

--- P0/Ex8.py
+++ P0/Ex8.py
@@ -1,13 +1,9 @@
 import P0.Seq0 as Seq0
 gene_folder = "./Sequences/"
 gene_list = ["U5", "ADA", "PRAT1", "FXN"]
-b = ["A", "C", "T", "G"]
-l = []
+
+
 print("-----| Exercise 8 |------")
 for gene in gene_list:
     sequence = Seq0.seq_read_fasta(gene_folder + gene + ".txt")
-    print("Gene " + gene)
-    for base in b:
-        bases = Seq0.seq_count_base(sequence, base)
-        l.append(bases)
-        print(base + ":", bases)
+    print("Gene ", gene, "most frequent base:", Seq0.seq_count(sequence))
