5d2c5e6a18a09a46eb3c8e5e06f349d4ef7daf44
vmalvarezcamarero <vmalvarezcamarerogmail.com>
2021-03-03 20:35:46 +0100

    ex 1 practice

---
+++ P1/Ex1.py
@@ -0,0 +1,4 @@
+from Seq1 import Seq , generate_series
+
+sequence = Seq("ACTGA")
+print(f"Sequence 1 (Length:{Seq.len(sequence)}): {sequence}")
---
+++ P1/Seq1.py
@@ -0,0 +1,41 @@
+class Seq:
+    """A class for representing sequences"""
+
+    def __init__(self, strbases):
+
+        self.strbases = strbases
+
+        if self.sequence_is_valid():
+            self.strbases = strbases
+            print("New sequence created!")
+        else:
+            self.strbases = "ERROR"
+            print("INCORRECT Sequence detected!")
+
+    def __str__(self):
+        """Method called when the object is being printed"""
+
+        # -- We just return the string with the sequence
+        return self.strbases
+
+    def sequence_is_valid(self):
+        for c in self.strbases:
+            if c != "A" and c != "C" and c != "G" and c != "T":
+                return False
+        return True
+
+
+    def len(self):
+        """Calculate the length of the sequence"""
+        return len(self.strbases)
+
+    @staticmethod
+    def print_bases(list_sequences):
+        for a in range(0,len(list_sequences)):
+            print(f"Sequence {a} (Length:{list_sequences[a].len()}):{list_sequences[a]}")
+
+def generate_series(sequ, number):
+    li = []
+    for i in range(0, number):
+        li.append(Seq(sequ * (i + 1)))
+    return li
