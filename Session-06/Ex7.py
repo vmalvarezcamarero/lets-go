1661ef95d7b857874156aa5a3925799c7ae0be66
vmalvarezcamarero <vmalvarezcamarerogmail.com>
2021-03-08 21:21:12 +0100

    avance ex 7

--- Session-06/Ex7.py
+++ Session-06/Ex7.py
@@ -3,6 +3,7 @@ print("-----| Practice 1, Exercise 6 |------")
 listt = ("A", "C", "T", "G")
 s1 = Seq()
 s2 = Seq("ACTGA")
+
 s3 = Seq("Invalid sequence")
 print(f"Sequence 1 (Length:{Seq.len(s1)}): {s1}\n Bases: {Seq.create_dict(s1)}\n Rev: {Seq.seq_reverse(s1)}")
 print(f"Sequence 2 (Length:{Seq.len(s2)}): {s2}\n Bases: {Seq.create_dict(s2)}\n Rev: {Seq.seq_reverse(s2)}")
