b235ab4e90be1a5fde2a8bae8901b778c800dc69
vmalvarezcamarero <vmalvarezcamarerogmail.com>
2021-03-16 19:51:09 +0100

    finished

---
+++ Session-08/Client.py
@@ -0,0 +1,24 @@
+import socket
+
+# SERVER IP, PORT
+PORT = 5050
+IP = "192.168.1.60"
+
+# First, create the socket
+# We will always use this parameters: AF_INET y SOCK_STREAM
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# establish the connection to the Server (IP, PORT)
+s.connect((IP, PORT))
+
+# Send data. No strings can be send, only bytes
+# It necesary to encode the string into bytes
+s.send(str.encode("HELLO FROM THE CLIENT!!!"))
+
+# Receive data from the server
+msg = s.recv(2048)
+print("<3>\n")
+print(msg.decode("utf-8"))
+
+# Closing the socket
+s.close()
