ba4ead786a34760ac45f16e8a8c10a25db1785bc
vmalvarezcamarero <78867548+vmalvarezcamarero@users.noreply.github.com>
2026-02-07 12:36:11 +0100

    Delete Session-04 directory

--- Session-04/body.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-FILENAME = "U5.txt"
-file_contents = Path(FILENAME).read_text()
-f = file_contents.split("\n")
-lis = f[1:]
-print("The sequence is:")
-for e in lis:
-    print (e)
