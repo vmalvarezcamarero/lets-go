0c2913edd30479e8a657fde29bfd0de5b5dff0fa
vmalvarezcamarero <78867548+vmalvarezcamarero@users.noreply.github.com>
2026-02-07 12:35:04 +0100

    Delete P0 directory

--- P0/Ex3.py
+++
@@ -1,9 +0,0 @@
-import P0.Seq0 as Seq0
-gene_folder = "./Sequences/"
-gene_list = ["U5", "ADA", "PRAT1", "FXN"]
-
-print("-----| Exercise 3 |------")
-for gene in gene_list:
-    sequence = Seq0.seq_read_fasta(gene_folder + gene + ".txt")
-    print("Gene " + gene + "---------> Length:" + str(Seq0.seq_len(sequence)))
-
--- P0/Ex6.py
+++
@@ -1,9 +0,0 @@
-import P0.Seq0 as Seq0
-FOLDER = "./Sequences/U5.txt"
-U5_Seq = Seq0.seq_read_fasta(FOLDER)
-
-print("-----| Exercise 6 |------")
-print("Gene U5:")
-print ("Frag:" + U5_Seq[0: 20])
-print ("Rev:" + str(Seq0.seq_reverse(U5_Seq[0: 20])))
-
